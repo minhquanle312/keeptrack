5d14cd52ec7998d26136db537eed3c7482c0f21b
minhquanle <minhquanle312@gmail.com>
2023-03-07 17:30:58 +0700

    lap 5

---
+++ src/projects/ProjectList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Project } from './Project'
+
+interface ProjectListProps {
+  projects: Project[]
+}
+
+function ProjectList() {
+  return <div>ProjectList</div>
+}
+
+export default ProjectList
