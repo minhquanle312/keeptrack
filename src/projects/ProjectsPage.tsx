1d96d6d284986b6b7c18ca099d56c4daa6ee46c6
Minh Quân Lê <minhquanle@gmail.com>
2023-03-07 23:52:44 +0700

    end lap C

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,16 @@
 import { MOCK_PROJECTS } from './MockProjects'
+import { Project } from './Project'
+import ProjectList from './ProjectList'
 
 function ProjectsPage() {
+  const saveProject = (project: Project) => {
+    console.log('Saving project: ', project)
+  }
+
   return (
     <>
       <h1>Projects</h1>
-      <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
+      <ProjectList projects={MOCK_PROJECTS} onSave={saveProject} />
     </>
   )
 }
